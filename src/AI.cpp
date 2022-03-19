30aca115a4d19a193b337e8436023a6ba63d8699
Vimal Kumar <vimal@Vimals-MacBook-Pro.local>
2022-03-19 12:34:18 -0700

    computer vs player

--- src/AI.cpp
+++ src/AI.cpp
@@ -17,30 +17,24 @@ void AI::AutoPlay() {
     int x=  static_cast<int>(snake->head_x);
     int y= static_cast<int>(snake->head_y);
 
-    if (x != food->x && y != food->y) return;
     if (x == food->x && y == food->y) return;
-
-//    SDL_Event sdlevent;
-//    sdlevent.type = SDL_KEYDOWN;
 
     if (x == food->x){
             if(y>food->y){
-//                sdlevent.key.keysym.sym = SDLK_UP;
+
                 snake->direction = Snake::Direction::kUp;
             }else{
-//                sdlevent.key.keysym.sym = SDLK_DOWN;
+
                 snake->direction = Snake::Direction::kDown;
             }
     }
     else {
         if(x>food->x){
-//            sdlevent.key.keysym.sym = SDLK_LEFT;
+
             snake->direction = Snake::Direction::kLeft;
         }else{
-//            sdlevent.key.keysym.sym = SDLK_RIGHT;
+
             snake->direction = Snake::Direction::kRight;
         }
     }
-
-//    SDL_PushEvent(&sdlevent);
 }
